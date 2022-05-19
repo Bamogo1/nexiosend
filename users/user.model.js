695bc813bf4ea10f5d3ecf6205974422f108d313
Ahmed <charifbam@gmail.com>
2022-05-19 13:14:25 +0200

    réussi v1

---
+++ users/user.model.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = model;
+
+function model(sequelize) {
+    const attributes = {
+        firstName: { type: DataTypes.STRING, allowNull: false },
+        lastName: { type: DataTypes.STRING, allowNull: false },
+        username: { type: DataTypes.STRING, allowNull: false },
+        hash: { type: DataTypes.STRING, allowNull: false },
+        telephone: { type: DataTypes.STRING, allowNull: false },
+        pays: { type: DataTypes.STRING, allowNull: false },
+        adresse: { type: DataTypes.STRING, allowNull: false },
+        etat: { type: DataTypes.STRING, allowNull: false }
+    };
+
+    const options = {
+        defaultScope: {
+            // exclude hash by default
+            attributes: { exclude: ['hash'] }
+        },
+        scopes: {
+            // include hash with this scope
+            withHash: { attributes: {}, }
+        }
+    };
+
+    return sequelize.define('User', attributes, options);
+}
